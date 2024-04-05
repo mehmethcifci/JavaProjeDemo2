5871c37c3516385ef2950ede2dc082f6eb2e702e
Mehmet Hasan Çifçi <103119140+mehmethcifci@users.noreply.github.com>
2024-04-05 11:58:29 +0300

    Update Program.java

--- src/JavaProjeDemo2/Program.java
+++ src/JavaProjeDemo2/Program.java
@@ -8,11 +8,11 @@ public class Program {
     	List<Personel> personelList = dosyaOku.readPersonelFromJSON("C:\\Users\\mehme\\eclipse-workspace\\JavaProjeDemo2\\src\\personel.json");
 
         MaasBordro maasBordro = new MaasBordro();
-        String monthYear = "SUBAT 2020"; // Bordro ayý ve yýlý
+        String monthYear = "SUBAT 2020"; // Bordro ayi ve yili
         List<Personel> azCalisanlar = maasBordro.generatePayroll(personelList, monthYear);
 
-        System.out.println("Maaþ bordrosu oluþturuldu ve kaydedildi.");
-        System.out.println("Az çalýþanlar:");
+        System.out.println("MaaÃ¾ bordrosu oluÃ¾turuldu ve kaydedildi.");
+        System.out.println("Az Ã§alÃ½Ã¾anlar:");
         maasBordro.printSummary(azCalisanlar);
     }
 }
